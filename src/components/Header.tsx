a05c0c7371ee5fa9f76afc7529176de17d6ca4b7
ivanab <ivana@opticaltone.com>
2024-03-22 15:00:36 +0100

    Show menu items

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,14 +2,28 @@ import React from "react";
 import { AppBar, Toolbar, Typography, Tabs, Tab, Button } from "@mui/material";
 import { NavbarWrapper } from "../styles/Header.modules";
 
+const menuItems = [
+  { name: "Home" },
+  { name: "Hackerspaces" },
+  { name: "Events" },
+];
+
 const Header = () => {
-  return <NavbarWrapper>
-    <AppBar>
+  return (
+    <NavbarWrapper>
+      <AppBar>
         <Toolbar>
-            <Typography>hackhub.nl</Typography>
+          <Typography>hackhub.nl</Typography>
+
+          <Tabs>
+            {menuItems.map((nav, index) => (
+              <Tab label={nav.name} />
+            ))}
+          </Tabs>
         </Toolbar>
-    </AppBar>
-  </NavbarWrapper>;
+      </AppBar>
+    </NavbarWrapper>
+  );
 };
 
 export default Header;
