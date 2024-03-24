b6686a23549a0cd1e8982eba0e5bfb71598a02de
ivanab <ivana@opticaltone.com>
2024-03-24 23:47:22 +0100

    API responded back with data

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
 import Header from "./components/Header";
 
 function App() {
+  // Testing API
+  const url = process.env.REACT_APP_API_URL as string;
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+
   return (
     <>
       <Header />
